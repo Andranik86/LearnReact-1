e729c5e4141f735ad33a006c62b0a69458185fbe
Andranik Ghevondyan <andranik_gh@instigatemobile.com>
2021-08-23 14:38:37 +0400

    implifie frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,11 @@ import './App.css';
 import {
   SERVER_URL,
 } from './constants'
-import { socket } from './services';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      fetchData: null,
-      socketData: null,
-    }
   }
   async componentDidMount() {
     const req = await fetch(`${SERVER_URL}/`, {
@@ -22,33 +17,13 @@ class App extends React.Component {
     })
     const { data } = await req.json()
     console.log(data)
-    this.setState({
-      fetchData: data,
-    })
-
-    socket.emit('data', (data) => this.setState({
-      socketData: data,
-    }))
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>Fetch data: {this.state.fetchData}</p>
-          <p>Socket data: {this.state.socketData}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Learn React
         </header>
       </div>
     );
